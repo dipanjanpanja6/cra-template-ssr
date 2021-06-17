b6e5bb967ca1186c3eef46cb01e4f6dea21d7079
Dipanjan Panja <dipanjan.panja@geogo.in>
2021-06-17 22:28:10 +0530

    update async await

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import path from "path"
 import template from "./src/template"
 import ssr from "./src/server"
 
+const fetch = require("node-fetch")
+
 const app = express()
 
 app.use("/assets", express.static(path.resolve(__dirname, "assets")))
@@ -11,12 +13,11 @@ app.use("/media", express.static(path.resolve(__dirname, "media")))
 app.disable("x-powered-by")
 app.listen(process.env.PORT || 3000)
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   //fetch call and sent data in ssr()
-  const { content } = ssr(someJsonData)
+  const x = await fetch("https://google.com")
+  const { content } = ssr(x)
   const response = template("Server Rendered Page", content)
   res.setHeader("Cache-Control", "assets, max-age=604800")
   res.send(response)
 })
-
-const someJsonData = { name: "React ssr" }
